b19f5772001b4c0892b154a395a5357cff2983fa
diepucdom <diepucdom@alu.edu.gva.es>
2026-01-31 20:31:24 +0100

    Primer commit

---
+++ hundirlaflota.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class hundirlaflota {
+    static Scanner sc = new Scanner(System.in);
+    public static char[][] crearTablero(int filas, int columnas) {
+    
+    }
+
+    public static void colocarBarcos(char[][] tablero, int numBarcos, int longitudBarco) {
+    
+    }
+    public static void mostrarTablero(char[][] tablero) {
+
+    }
+
+
+    public static void hundirLaFlota(char[][] tableroA, char[][] tableroB) {
+
+    }
+
+
+    public static void main(String[] args) {
+    
+    }
+}
